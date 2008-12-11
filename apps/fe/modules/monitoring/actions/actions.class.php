e3f59a68cd211c29c4ede403c8f2b4f3fe11118c
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2008-12-11 12:30:28 +0000

    risolto bug quando l'elenco degli atti bloccati e' vuoto
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@78 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/monitoring/actions/actions.class.php
+++ apps/fe/modules/monitoring/actions/actions.class.php
@@ -70,7 +70,7 @@ class monitoringActions extends sfActions
     
     // eliminazione delle notizie relative agli oggetti bookmarkati negativamente (bloccati)
     $blocked_items_ids = sfBookmarkingPeer::getAllNegativelyBookmarkedIds($this->user_id);
-    if (array_key_exists($blocked_items_ids, 'OppAtto') && count($blocked_items_ids['OppAtto']))
+    if (array_key_exists('OppAtto', $blocked_items_ids) && count($blocked_items_ids['OppAtto']))
     {
       $blocked_news_ids = array();
       $bc = new Criteria();
