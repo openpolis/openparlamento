634fe7135d3d2aff389bf857fc86727f969f5edb
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-01-27 11:05:26 +0000

    modificata l'inclusione della jQuery, ora e' zippata
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@112 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/updateFirmeNews.php
+++ batch/updateFirmeNews.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Openpolis project
  *
- * (c) 2008 Guglielmo Celata <guglielmo.celata@gmail.com>
+ * (c) 2009 Guglielmo Celata <guglielmo.celata@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
