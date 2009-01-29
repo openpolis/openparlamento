3d6058e6047b9cf4039a5b13e488802693701ee4
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-01-29 10:15:25 +0000

    aggiunto menu di secondo livello
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@121 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ apps/fe/modules/parlamentare/templates/_secondlevelmenu.php
@@ -0,0 +1,12 @@
+<div class="float-container">
+	<ul class="second-level-tabs float-container">
+		<li class="current"><h5><a href="singolo_politico_cosa_fa.html">Cosa fa</a></h5>
+		</li>
+		<li><h5><a href="singolo_politico_atti.html">Atti presentati</a></h5>
+		</li>
+		<li><h5><a href="singolo_politico_voti.html">Come ha votato</a></h5>
+		</li>
+		<li><h5><a href="singolo_politico_interventi.html">Interventi parlamentari</a></h5>
+		</li>
+	</ul>
+</div>
