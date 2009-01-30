187eb88b334e8ab14483465dfe6003b3ddf965f4
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-01-30 18:58:30 +0000

    modifiche al css
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@126 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/argomento/templates/listSuccess.php
+++ apps/fe/modules/argomento/templates/listSuccess.php
@@ -1,3 +1,4 @@
 <?php foreach($argomenti as $argomento): ?>
-  <div><?php echo link_to($argomento->getDenominazione(),'@argomento?id='.$argomento->getId()) ?></div>
+     <div><?php echo link_to($argomento->getDenominazione(),'@argomento?id='.$argomento->getId()) ?></div>
 <?php endforeach; ?>
+               
