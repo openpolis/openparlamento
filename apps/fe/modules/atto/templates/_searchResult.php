e57c0388b7199e9be9c5380fd385fa1d6e9b58e4
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-03-25 18:04:35 +0000

    risolto problema su risultati di ricerca quando tra i risultati c'e' un politico (#189)
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@205 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/atto/templates/_searchResult.php
+++ apps/fe/modules/atto/templates/_searchResult.php
@@ -1,4 +1,4 @@
-ATTO::<?php echo $result->tipo_atto_s ?>::<?php echo link_to(highlight_keywords($result->titolo, $query, sfConfig::get('app_lucene_result_highlighter', '<strong class="highlight">%s</strong>')), add_highlight_qs($result->getInternalUri(), $query)) ?> (<?php echo $result->getScore() ?>%)
+ATTO::<?php echo OppTipoAttoPeer::retrieveByPK($result->tipo_atto_id)->getDescrizione() ?>::<?php echo link_to(highlight_keywords($result->titolo, $query, sfConfig::get('app_lucene_result_highlighter', '<strong class="highlight">%s</strong>')), add_highlight_qs($result->getInternalUri(), $query)) ?> (<?php echo $result->getScore() ?>%)
 
 <?php if ($result->hasDescrizioneWiki == 'true'): ?>
   <p style="margin-top: 5px; margin-bottom: 10px; color: #666"><?php echo highlight_result_text(OppAttoPeer::retrieveByPK($result->propel_id)->getDescrizioneWiki(), $query, sfConfig::get('app_lucene_result_size', 200), sfConfig::get('app_lucene_result_highlighter', '<strong class="highlight">%s</strong>')) ?></p>  
---
+++ apps/fe/modules/parlamentare/templates/_searchResult.php
@@ -0,0 +1,4 @@
+POLITICO::<?php echo link_to(highlight_keywords($result->nominativo, $query, 
+                                                sfConfig::get('app_lucene_result_highlighter', 
+                                                              '<strong class="highlight">%s</strong>')), 
+                             add_highlight_qs($result->getInternalUri(), $query)) ?> (<?php echo $result->getScore() ?>%)
