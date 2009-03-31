9ac75ad3015260ef76816d3c582aa6407abcb860
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-03-31 13:19:25 +0000

    aggiunto modulo pagine statiche
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@212 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ apps/fe/modules/static/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * static actions.
+ *
+ * @package    op_openparlamento
+ * @subpackage static
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
+ */
+class staticActions extends sfActions
+{
+  public function executeProgetto(){}
+  
+}
---
+++ apps/fe/modules/static/templates/progettoSuccess.php
@@ -0,0 +1,14 @@
+<div id="content" class="tabbed float-container">
+
+  <div id="main">
+    <div class="W100_100">
+      Questa &egrave; la paggina der Progggetto.
+    </div>
+  </div>
+
+</div>
+
+<?php slot('breadcrumbs') ?>
+  <?php echo link_to("home", "@homepage") ?> /
+  progetto
+<?php end_slot() ?>
