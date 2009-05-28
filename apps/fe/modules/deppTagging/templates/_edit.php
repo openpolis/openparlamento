69786549225096aee859879c8734f78bcb418c06
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-05-28 14:41:30 +0000

    corretto bug sparizione tool di rimozione tag per gli atti
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@262 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/deppTagging/templates/_edit.php
+++ apps/fe/modules/deppTagging/templates/_edit.php
@@ -20,7 +20,7 @@ jQuery(document).ready(function(){
     <em>argomenti:</em>
     <?php foreach ($tags as $tag): ?>
       <div id="<?php echo $tag[0]?>" class="<?php echo get_classes_for_tag($tag[3], $teseo_tags, $user_tags, $my_tags) ?>">
-        <?php if ($sf_user->hasCredential('administrator')): ?>
+        <?php if ($sf_user->hasCredential('amministratore')): ?>
           <span class="remover" title="clicca qui per rimuovere questo tag">(X)</span>        
         <?php endif ?>
         <span class="tag"><?php echo link_to(strtolower($tag[3]), '@argomento?triple_value='.$tag[3])?></span>
