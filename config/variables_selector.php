c2d85513a1bbc6a05521242c211573f31f13faca
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-06-03 15:33:56 +0000

    modificato script di pre-configurazione variabili
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@271 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- config/variables_selector.php
+++ config/variables_selector.php
@@ -1,7 +1,8 @@
 <?php
 if (array_key_exists('SERVER_NAME', $_SERVER))
 {
-  if ($_SERVER['SERVER_NAME'] == 'op_openparlamento.openpolis.it') {
+  if ($_SERVER['SERVER_NAME'] == 'op_openparlamento.openpolis.it') 
+  {
     $site_url = 'op_openparlamento.openpolis.it';
     $remote_guard_host = 'op_accesso.openpolis.it';
     $op_openpolis_host = 'op_openpolis.openpolis.it';
@@ -21,21 +22,19 @@ if (array_key_exists('SERVER_NAME', $_SERVER))
 } else {
   if (array_key_exists('SERVER_IP', $_SERVER))
   {
-    if ($_SERVER['SERVER_IP']=='195.94.177.111')
-    {
+    if ($_SERVER['SERVER_IP']=='195.94.177.111') {
       $site_url = 'parlamentodev.openpolis.it';
       $remote_guard_host = 'accessodev.openpolis.it';
-      $op_openpolis.it = 'dev.openpolis.it';
+      $op_openpolis_host = 'dev.openpolis.it';
     } else if ($_SERVER['SERVER_IP']=='94.23.26.161') {
       $site_url = 'parlamento.openpolis.it';
       $remote_guard_host = 'accesso.openpolis.it';
-      $op_openpolis.it = 'www.openpolis.it';
-    } else ($_SERVER['HOSTNAME']=='lapgu.local') {
+      $op_openpolis_host = 'www.openpolis.it';
+    } else {
       $site_url = 'op_openparlamento.openpolis.it';
       $remote_guard_host = 'op_accesso.openpolis.it';
-      $op_openpolis.it = 'op_openpolis.openpolis.it';
+      $op_openpolis_host = 'op_openpolis.openpolis.it';
     }
   }
 }
-$api_key = '3114a2d106054d26c364c4cfff85910f97f7e29a';
 ?>
