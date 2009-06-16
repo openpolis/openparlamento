2078d075984916e88065f7185c73ac08d93703ea
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-06-16 09:19:12 +0000

    rimossa doppia tabella in pagina servizi
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@359 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/default/templates/sottoscrizioniProSuccess.php
+++ apps/fe/modules/default/templates/sottoscrizioniProSuccess.php
@@ -15,11 +15,12 @@
                 <div class="flash-messages">
                   <?php echo $sf_flash->get('subscription_limit_reached') ?>
                 </div>
-              <?php endif; ?>
+              <?php endif; ?> 
               
               <div class="intro-box">
                   <h2>Qual &egrave; il <em class="open">tuo</em><em class="parlamento">parlamento</em>?</h2>
-                  <p><em class="open">open</em><em class="parlamento">parlamento</em> consegna nella tua e-mail e nelle pagine web a te dedicate le informazioni che ti interessano nei modi e nelle forme che preferisci. Servizi di informazione e monitoraggio sulle attivit&agrave; parlamentari per <strong>cittadini, professionisti</strong> e qualunque tipo di <strong>organizzazione</strong>.</p>
+                  <p>Tutti i contenuti di <em class="open">open</em><em class="parlamento">parlamento</em> sono <strong>liberamente consultabili</strong> senza la necessit&agrave; di alcuna registrazione.</p>
+                  <p>Se sei un utente registrato, <em class="open">open</em><em class="parlamento">parlamento</em> consegna nella tua e-mail e nelle pagine web a te dedicate le informazioni che ti interessano nei modi e nelle forme che preferisci.<br /> Servizi di informazione e monitoraggio sulle attivit&agrave; parlamentari per <strong>cittadini, professionisti</strong> e qualunque tipo di <strong>organizzazione</strong>.</p>
               </div>
             </div>
             
@@ -70,83 +71,80 @@
               </div>
             <?php endif ?>
             <div class="clear-both"></div>
-            
-        </div>
-
-
-        <div class="W100_100">
-            <table class="compare-table">
-                <tr>
-                    <td colspan="2" rowspan="2">&nbsp;</td>
-                    <td colspan="4" class="pad0"><h4 class="grey-box-contrast round-5-top">servizi disponibili</h4></td>
-                    <td rowspan="2" class="pad0"><h4 class="grey-box-contrast round-5-top">modalit&agrave;<br />
-                            di accesso</h4></td>
-                </tr>
-                <tr>
-                    <td class="pad0"><h6 class="grey-box round-5-top">monitoraggio atti e parlamentari</h6></td>
-                    <td class="pad0"><h6 class="grey-box round-5-top">monitoraggio argomenti</h6></td>
-                    <td class="pad0"><h6 class="grey-box round-5-top">altri servizi personalizzati</h6></td>
-                    <td class="pad0"><h6 class="grey-box round-5-top">account multipli</h6></td>
-                </tr>
-                <tr>
-                    <td class="grey-box-contrast round-5-left" rowspan="3"><img src="/images/txt-tipologie-utente.png" width="18" height="134" alt="tipologie utente" /></td>
-                    <td class="align-left bg-flatgreen-dark round-5-left"><img src="/images/ico-citizen.png" alt="CITIZEN" class="user-type" /></td>
-                    <td class="bg-flatgreen-light">MAX 5</td>
-                    <td class="bg-flatgreen-light">--</td>
-                    <td class="bg-flatgreen-light">--</td>
-                    <td class="bg-flatgreen-light">--</td>
-                    <td class="bg-flatgreen-light round-5-right"><p><br />
-                            <br />
-                            GRATUITA</p>
-                            <?php if (!$sf_user->hasCredential('subscriber')): ?>
+         <div class="W100_100">
+                <table class="compare-table">
+                    <tr>
+                        <td colspan="2" rowspan="2">&nbsp;</td>
+                        <td colspan="4" class="pad0"><h4 class="grey-box-contrast round-5-top">servizi disponibili</h4></td>
+                        <td rowspan="2" class="pad0"><h4 class="grey-box-contrast round-5-top">modalit&agrave;<br />
+                                di accesso</h4></td>
+                    </tr>
+                    <tr>
+                        <td class="pad0"><h6 class="grey-box round-5-top">monitoraggio atti e parlamentari</h6></td>
+                        <td class="pad0"><h6 class="grey-box round-5-top">monitoraggio argomenti</h6></td>
+                        <td class="pad0"><h6 class="grey-box round-5-top">altri servizi personalizzati</h6></td>
+                        <td class="pad0"><h6 class="grey-box round-5-top">account multipli</h6></td>
+                    </tr>
+                    <tr>
+                        <td class="grey-box-contrast round-5-left" rowspan="3"><img src="/images/txt-tipologie-utente.png" width="18" height="134" alt="tipologie utente" /></td>
+                        <td class="align-left bg-flatgreen-dark round-5-left"><img src="/images/ico-citizen.png" alt="CITIZEN" class="user-type" /></td>
+                        <td class="bg-flatgreen-light">MAX 5</td>
+                        <td class="bg-flatgreen-light">--</td>
+                        <td class="bg-flatgreen-light">--</td>
+                        <td class="bg-flatgreen-light">--</td>
+                        <td class="bg-flatgreen-light round-5-right"><p><br />
+                                <br />
+                                GRATUITA</p>
+                                <?php if (!$sf_user->hasCredential('subscriber')): ?>
+                                  <h5 class="launch-evidence-btn-mini round-5">
+                                    <?php echo link_to('registrati!', '@sf_guard_signin') ?>
+                                  </h5>
+                                <?php endif ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-left bg-green-dark round-5-left"><img src="/images/ico-premium.png" alt="PREMIUM" class="user-type" /></td>
+                        <td class="bg-green-light">MAX 10</td>
+                        <td class="bg-green-light">MAX 3</td>
+                        <td class="bg-green-light">--</td>
+                        <td class="bg-green-light">--</td>
+                        <td class="bg-green-light round-5-right"><p>in promozione<br />
+                                GRATUITA fino al<br />
+                                15 Ottobre<br />
+                                <?php if (!$sf_user->isAuthenticated()): ?>
+                                  per utenti <?php echo link_to('registrati', '@sf_guard_signin') ?>
+                                <?php endif ?>
+                            </p>
+                            
+                            <?php if ($sf_user->hasCredential('subscriber') && !$sf_user->hasCredential('premium') && !$sf_user->hasCredential('adhoc')): ?>
                               <h5 class="launch-evidence-btn-mini round-5">
-                                <?php echo link_to('registrati!', '@sf_guard_signin') ?>
+                                <?php echo link_to('aderisci!', '@sottoscrizione_premium_demo') ?>
                               </h5>
                             <?php endif ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="align-left bg-green-dark round-5-left"><img src="/images/ico-premium.png" alt="PREMIUM" class="user-type" /></td>
-                    <td class="bg-green-light">MAX 10</td>
-                    <td class="bg-green-light">MAX 3</td>
-                    <td class="bg-green-light">--</td>
-                    <td class="bg-green-light">--</td>
-                    <td class="bg-green-light round-5-right"><p>in promozione<br />
-                            GRATUITA fino al<br />
-                            15 Ottobre<br />
-                            <?php if (!$sf_user->isAuthenticated()): ?>
-                              per utenti <?php echo link_to('registrati', '@sf_guard_signin') ?>
-                            <?php endif ?>
-                        </p>
-                        
-                        <?php if ($sf_user->hasCredential('subscriber') && !$sf_user->hasCredential('premium') && !$sf_user->hasCredential('adhoc')): ?>
-                          <h5 class="launch-evidence-btn-mini round-5">
-                            <?php echo link_to('aderisci!', '@sottoscrizione_premium_demo') ?>
-                          </h5>
-                        <?php endif ?>
-                  </td>
-                      
-                </tr>
-                <tr>
-                    <td class="align-left bg-cyan-dark round-5-left"><img src="/images/ico-adhoc.png" alt="AD HOC" class="user-type" /></td>
-                    <td class="bg-cyan-light round-5-bottom">SENZA LIMITI</td>
-                    <td class="bg-cyan-light round-5-bottom">SENZA LIMITI</td>
-                    <td class="bg-cyan-light round-5-bottom">SVILUPPATI AD HOC</td>
-                    <td class="bg-cyan-light round-5-bottom">S&Iacute;</td>
-                    <td  class="bg-cyan-light round-5-no-tl"><p>condizioni<br />
-                            da concordare<br />
-                            direttamente<br />
-                        </p>
-                        <h5 class="launch-evidence-btn-mini round-5">
-                          <?php echo link_to('contattaci!', '@contatti') ?>
-                        </h5>
-                    </td>
-                </tr>
-            </table>
-        </div>
+                      </td>
+                          
+                    </tr>
+                    <tr>
+                        <td class="align-left bg-cyan-dark round-5-left"><img src="/images/ico-adhoc.png" alt="AD HOC" class="user-type" /></td>
+                        <td class="bg-cyan-light round-5-bottom">SENZA LIMITI</td>
+                        <td class="bg-cyan-light round-5-bottom">SENZA LIMITI</td>
+                        <td class="bg-cyan-light round-5-bottom">SVILUPPATI AD HOC</td>
+                        <td class="bg-cyan-light round-5-bottom">S&Iacute;</td>
+                        <td  class="bg-cyan-light round-5-no-tl"><p>condizioni<br />
+                                da concordare<br />
+                                direttamente<br />
+                            </p>
+                            <h5 class="launch-evidence-btn-mini round-5">
+                              <?php echo link_to('contattaci!', '@contatti') ?>
+                            </h5>
+                        </td>
+                    </tr>
+                </table>
+            </div>   
+       </div>
 
-
-  </div>
+         
+     </div>
 </div>
 
 <?php slot('breadcrumbs') ?>
