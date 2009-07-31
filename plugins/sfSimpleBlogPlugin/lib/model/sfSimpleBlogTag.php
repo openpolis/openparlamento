4ca8fb185dde05f7d696baab2027944b3ec537ef
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-07-31 11:20:52 +0000

    eliminati override nei metodi save del Blog, la soluzione per pulire la cache e' pie' complessa
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@394 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- plugins/sfSimpleBlogPlugin/lib/model/sfSimpleBlogTag.php
+++ plugins/sfSimpleBlogPlugin/lib/model/sfSimpleBlogTag.php
@@ -9,37 +9,4 @@
  */ 
 class sfSimpleBlogTag extends PluginsfSimpleBlogTag
 {
-  /**
-   * override per modificare la priorità dei comunicati di governo
-   *
-   * @param string $con 
-   * @return void
-   * @author Guglielmo Celata
-   */
-  public function save($con = null)
-  {
-    
-    $this->clearCacheOnUpdate();
-    
-    return parent::save();
-
-  }
-
-  /**
-   * clear some cached stuff before the object is modified
-   *
-   * @return void
-   * @author Guglielmo Celata
-   */
-  public function clearCacheOnUpdate()
-  {
-    $cacheManager = sfContext::getInstance()->getViewCacheManager();
-    if ($cacheManager)
-    {
-      $cacheManager->remove('sfSimpleBlog/show?id='.$this->getSfBlogPostId());
-      $cacheManager->remove('sfSimpleBlog/index');
-    }
-    
-  }
-  
 }
