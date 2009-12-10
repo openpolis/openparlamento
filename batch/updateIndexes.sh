cfe4f75d10e7a4ee1bb0e1b9bf2f9284b4f2edf4
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-12-10 14:58:45 +0000

    aggiunto update dei resoconti all'update degli indici
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@526 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/updateIndexes.sh
+++ batch/updateIndexes.sh
@@ -3,4 +3,5 @@ php symfony solr-update-propel-model fe OppPolitico 1000
 php symfony solr-update-propel-model fe OppVotazione 200
 php symfony solr-update-propel-model fe OppDocumento 200
 php symfony solr-update-propel-model fe OppAtto 500
+php symfony solr-update-propel-model fe OppResoconto 500
 php symfony solr-update-propel-model fe Tag 1000
