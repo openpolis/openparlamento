7ceb6ffe64e73ff59ee96bd8daf92a6f2589de9b
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-12-18 19:52:57 +0000

    utility per assegnare voto a gruppi
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@549 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ batch/utility/votoGruppiReview.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+Lo script controlla se ci sono errori nella tabella opp_votazione_has_gruppo, ovvero prende tutti i valori voto=nv e li verifica.
+Prende in input il numero della legislatura
+*/
+ 
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/../..'));
+define('SF_APP',         'fe');
+define('SF_ENVIRONMENT', 'dev');
+define('SF_DEBUG',       false);
+ 
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+sfContext::getInstance();
+
+$c= new Criteria();
+$c-> addJoin(OppSedutaPeer::ID,OppVotazionePeer::SEDUTA_ID);
+$c-> addJoin(OppVotazioneHasGruppoPeer::VOTAZIONE_ID,OppVotazionePeer::ID);
+$c-> add(OppSedutaPeer::LEGISLATURA,$argv[1]);
+$c-> add(OppVotazioneHasGruppoPeer::VOTO,'nv');
+$c-> addGroupByColumn(OppVotazionePeer::ID);
+$votazioni = OppVotazionePeer::doSelect($c);
+
+foreach($votazioni as $votazione)
+{
+  print "elaborazione votazione: " . $votazione->getId() . "...\n";
+	
+  $c = new Criteria();
+  $c->add(OppLegislaturaHasGruppoPeer::LEGISLATURA, $argv[1], Criteria::EQUAL);
+  $c->add(OppLegislaturaHasGruppoPeer::RAMO, $votazione->getOppSeduta()->getRamo(), Criteria::EQUAL);
+  $gruppi_votazione = OppLegislaturaHasGruppoPeer::doSelect($c);
+    
+	foreach ($gruppi_votazione as $gruppo)
+	{
+	  $gr = OppGruppoPeer::retrieveByPk($gruppo->getGruppoId());	  
+	  $voto_gruppo = OppVotazionePeer::doSelectVotoGruppo($votazione->getId(), $gr->getNome());	
+	  
+	  $c= new Criteria();
+	  $c->add(OppVotazioneHasGruppoPeer::VOTAZIONE_ID,$votazione->getId());
+	  $c->add(OppVotazioneHasGruppoPeer::GRUPPO_ID,$gr->getId());
+	  $result=OppVotazioneHasGruppoPeer::doSelectOne($c);
+	  if ($result)
+	  {
+	    $result->setVoto($voto_gruppo);
+	    $result->save();
+	    print $gr->getNome().": ".$voto_gruppo."\n";	  
+	  } 
+	}	
+  
+}
+
+
+print("done.\n");
+
+?>
