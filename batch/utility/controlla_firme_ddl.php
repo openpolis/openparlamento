6b28618f31dcbc5168f93ce93e2bc727708b3bc4
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2009-12-30 12:37:35 +0000

    refinings output
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@571 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/utility/controlla_firme_ddl.php
+++ batch/utility/controlla_firme_ddl.php
@@ -32,7 +32,11 @@ foreach ($results as $result)
     $aka=OppAppoggioPeer::doSelectOne($c);
     if ($aka)
       $url="http://www.senato.it/ricerche/sDDLa/risultati.ricerca?searchID=null&parms.output=&parms.statoDiv=0%2C0%2C0%2C1%2C0%2C0%2C0&parms.statoIcone=0%2C0%2C0%2C1%2C0%2C0%2C0&parms.sel=&parms.des=&parms.selmode=&parms.legislatura=".$argv[1]."&parms.ramo=+&parms.numeroFase=&parms.specification=&parms.testoScheda=&parms.natura=+&parms.lettura=+&parms.presentatoDal=&parms.presentatoAl=&parms.numeroDecretoLegge=&parms.decretoLeggeDel=&parms.numeroGazzettaDecreto=&parms.gazzettaDecretoDel=&parms.numeroLegge=&parms.leggeDel=&parms.numeroGazzettaLegge=&parms.gazzettaLeggeDel=&parms.statoDal=&parms.statoAl=&parms.tuttiTermini=T&parms.teseo=&parms.livelloTeseo=+&parms.naturaParlamentare=on&parms.deputatoIniziativa=".$aka->getAka()."&parms.iniziativa=&parms.tipoFirmatari=+&parms.assegnazione=&parms.ultimaAssegnazione=on&parms.assegnatoDal=&parms.assegnatoAl=&parms.pareri=&parms.criterioPareriAssegnazione=OR&parms.trattazione=&parms.trattatoDal=&parms.trattatoAl=&parms.fattoProprio=&parms.relatori=&parms.options.resultsPerPage=10&parms.ordinamento=DESC&parms.ordinaPerData=true&button-cerca=Cerca";
-    else echo "??? non trovo AKA per ".$result->getId()."\n";
+    else 
+    {
+      echo "??? non trovo AKA per ".$result->getId()."\n";
+      $url="";
+    }  
   }
   else
   {
@@ -40,47 +44,47 @@ foreach ($results as $result)
     $url="http://www.senato.it/ricerche/sDDLa/risultati.ricerca?searchID=null&parms.output=&parms.statoDiv=0%2C0%2C0%2C1%2C0%2C0%2C0&parms.statoIcone=0%2C0%2C0%2C1%2C0%2C0%2C0&parms.sel=&parms.des=&parms.selmode=&parms.legislatura=".$argv[1]."&parms.ramo=+&parms.numeroFase=&parms.specification=&parms.testoScheda=&parms.natura=+&parms.lettura=+&parms.presentatoDal=&parms.presentatoAl=&parms.numeroDecretoLegge=&parms.decretoLeggeDel=&parms.numeroGazzettaDecreto=&parms.gazzettaDecretoDel=&parms.numeroLegge=&parms.leggeDel=&parms.numeroGazzettaLegge=&parms.gazzettaLeggeDel=&parms.statoDal=&parms.statoAl=&parms.tuttiTermini=T&parms.teseo=&parms.livelloTeseo=+&parms.naturaParlamentare=on&parms.senatoreIniziativaElencoTotale=".$parl_id."&parms.iniziativa=&parms.tipoFirmatari=+&parms.assegnazione=&parms.assegnatoDal=&parms.assegnatoAl=&parms.pareri=&parms.criterioPareriAssegnazione=OR&parms.trattazione=&parms.trattatoDal=&parms.trattatoAl=&parms.fattoProprio=&parms.relatori=&parms.options.resultsPerPage=10&parms.ordinamento=DESC&parms.ordinaPerData=true&button-cerca=Cerca";
   }
   
-  for ($i = 1; $i <= 10; $i++)
+  if ($url!="")
   {
-    try 
+    for ($i = 1; $i <= 10; $i++)
     {
-  	  $html = file_get_html($url);
-  	  if (!@file_get_html($url))
-  		  throw new Exception('$a è negativo');
-  	  break;
+      try 
+      {
+    	  $html = file_get_html($url);
+    	  if (!@file_get_html($url))
+    		  throw new Exception('$a è negativo');
+    	  break;
+      }
+    	catch (Exception $e) 
+    	{
+    	  //echo 'Caught exception: ',  $e->getMessage(), "\n";
+      }
+    } 
+
+    echo $result->getParliamentId()." ";
+    $errore=$html->find('p.messaggioErrore');
+    $numero=-1;
+
+    foreach ($errore as $er)
+    {
+      $numero=0;
     }
-  	catch (Exception $e) 
-  	{
-  	  //echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+    if ($numero==-1)
+    {
+      $numero=$html->find('div.rigaTabRic div.sottoTit');
+      $numero=$numero[0]->find('strong');
+      $numero=$numero[2]->plaintext;
     }
-  } 
-  
-  echo $result->getParliamentId()." ";
-  $errore=$html->find('p.messaggioErrore');
-  $numero=-1;
-  
-  foreach ($errore as $er)
-  {
-    echo $numero=0;
+    $c= new Criteria();
+    $c->addJoin(OppCaricaHasAttoPeer::ATTO_ID,OppAttoPeer::ID);
+    $c->add(OppAttoPeer::TIPO_ATTO_ID,1);
+    $c->add(OppCaricaHasAttoPeer::CARICA_ID,$result->getId());
+    $c->add(OppCaricaHasAttoPeer::TIPO,'R',Criteria::NOT_EQUAL);
+    $count=OppCaricaHasAttoPeer::doCount($c);
+    if ($numero<$count) echo "!!!! ERRORE in ".$result->getId()." nel DB n.".$count." nel sito ".$numero."\n";
+    if ($numero>$count) echo "!!!! ERRORE MANCANO FIRME in ".$result->getId()." nel DB n.".$count." nel sito ".$numero."\n";
   }
-  
-  if ($numero==-1)
-  {
-    $numero=$html->find('div.rigaTabRic div.sottoTit');
-    $numero=$numero[0]->find('strong');
-    echo $numero=$numero[2]->plaintext;
-  }
-  $c= new Criteria();
-  $c->addJoin(OppCaricaHasAttoPeer::ATTO_ID,OppAttoPeer::ID);
-  $c->add(OppAttoPeer::TIPO_ATTO_ID,1);
-  $c->add(OppCaricaHasAttoPeer::CARICA_ID,$result->getId());
-  $c->add(OppCaricaHasAttoPeer::TIPO,'R',Criteria::NOT_EQUAL);
-  $count=OppCaricaHasAttoPeer::doCount($c);
-  echo " - ".$count."\n";
-  if ($numero<$count) echo "!!!! ERRORE in ".$result->getId()." nel DB n.".$count." nel sito ".$numero."\n";
-  if ($numero>$count) echo "!!!! ERRORE MANCANO FIRME in ".$result->getId()." nel DB n.".$count." nel sito ".$numero."\n";
-  
-  
 }
 
 
