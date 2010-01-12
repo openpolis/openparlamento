9c0ba00e1742e33807d124f8ffe081f59ac0d705
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-01-12 13:01:27 +0000

    add controllo se succ in lista dl esiste
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@629 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/atto/templates/_ddlConversione.php
+++ apps/fe/modules/atto/templates/_ddlConversione.php
@@ -1,3 +1,3 @@
-<?php if($ddl->getSucc()): ?>
+<?php if($ddl_di_conversione && $ddl->getSucc()): ?>
   <?php echo link_to($ddl_di_conversione->getRamo().'.'.$ddl_di_conversione->getNumfase(), 'atto/index?id='.$ddl_di_conversione->getId()) ?> 	   
 <?php endif; ?>
