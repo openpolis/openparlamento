373d4eea4e571c1b274777da4efd24eb3d648f4f
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-01-15 17:03:09 +0000

    add ancora a rappresentometro
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@663 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/monitoring/templates/politiciansSuccess.php
+++ apps/fe/modules/monitoring/templates/politiciansSuccess.php
@@ -66,8 +66,8 @@
     <?php else: ?>
       	<h5 class="subsection">i parlamentari che stai monitorando</h5>
       <p style="font-size:14px; padding:10px;">Non stai monitorando nessun parlamentare.<br /> Per avviare un monitoraggio vai nella pagina di un parlamentare (<?php echo link_to('deputati','/parlamentari/camera/nome/asc') ?> e <?php echo link_to('senatori','/parlamentari/senato/nome/asc') ?>) e clicca su "avvia il monitoraggio" che trovi nella parte destra della pagina.</p>     
-    <a name="rappresentometro">&nbsp;</a>  
     <?php endif ?>
+    <a name="rappresentometro">&nbsp;</a>  
   
    <?php echo include_component('monitoring', 'userVspolitician', array('user' => $sf_user, 'num'=> 10, 'ambient' =>'monitor')); ?>
     
