7fbdb1b21bcad4eba9d211c003c247a7e944ffa8
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-01-19 07:25:19 +0000

    trap dell'errore su risultati alert di tipo Esito non corrispondenti a nessun oggetto nel DB
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@667 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/atto/templates/_alertEsito.php
+++ apps/fe/modules/atto/templates/_alertEsito.php
@@ -1,7 +1,19 @@
 <?php 
   $esito = OppEsitoSedutaPeer::retrieveByPK($result->propel_id);
-  $atto = $esito->getOppAtto();
-  $sede = $esito->getOppSede();
+  if ($esito instanceof OppEsitoSedutaPeer)
+  {
+    $atto = $esito->getOppAtto();
+    $sede = $esito->getOppSede();    
+  }
 ?>
+
+<?php if ($esito instanceof OppEsitoSedutaPeer): ?>
+  <?php
+    $atto = $esito->getOppAtto();
+    $sede = $esito->getOppSede();      
+  ?>
   <?php echo $sede->getRamo() ?>  - esito <?php echo $esito->getId() ?> in <?php echo $sede->getDenominazione() ?>, riferito ad atto <?php echo $atto->getId() ?> (da modificare)
+<?php else: ?>
+  Errore! Il risultato <?php echo $result->propel_id ?> si riferisce a un oggetto non più presente nel DB
+<?php endif ?>
 
