65572529fdb4f29d5b6ab821710a5c8d49c51920
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-02-03 11:59:18 +0000

    corretto problema nell'inserimento di documenti allegati ad atti in backend (#380)
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@700 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/be/modules/documento/templates/_atto_id.php
+++ apps/be/modules/documento/templates/_atto_id.php
@@ -1,4 +1,4 @@
 <?php $value = object_input_tag($opp_documento, 'getAttoId', array (
                                 'size' => 40,
-                                'control_name' => 'opp_documento[atto_it]',
+                                'control_name' => 'opp_documento[atto_id]',
                                )); echo $value ? $value : '&nbsp;' ?>
