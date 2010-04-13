12e84e14103c759709766bdd1f20574610df3db0
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-04-13 16:52:51 +0000

    add pagina statistiche ddl
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@783 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/atto/templates/_ddl2legge.php
+++ apps/fe/modules/atto/templates/_ddl2legge.php
@@ -10,7 +10,7 @@
   <?php if ($i==2): ?>
     <?php echo "Popolo - " ?>
   <?php endif ?>
-  <?php echo "Presentati: ".$arr[0]." - Leggi: ".$arr[1]." - % di successo: ".number_format($arr[1]*100/$arr[0],2)." - tempo medio: <strong>".$arr[2]." giorni</strong>" ?>
+  <?php echo "Presentati: ".$arr[0]." - Leggi: ".$arr[1]." - % di successo: ".number_format($arr[1]*100/$arr[0],2)." - tempo medio: ".$arr[2]." giorni" ?>
   <?php echo "<br/>" ?>
 <?php endforeach ?>
 <br /><br />
@@ -19,7 +19,7 @@
 <?php //$veloci=array_slice($arr_alls, 0, 20) ?>
 <?php $veloci=$arr_alls ?>
 <?php foreach ($veloci as $veloce ) : ?>
-  <?php echo "- ". link_to($veloce[0]->getRamo().".".$veloce[0]->getNumfase(),"/singolo_atto/".$veloce[0]->getId())." - ".$veloce[0]->getTitolo()." - tempo medio: <strong>".intval($veloce[1]."</strong>") ?>
+  <?php echo "- ". link_to($veloce[0]->getRamo().".".$veloce[0]->getNumfase(),"/singolo_atto/".$veloce[0]->getId())." - ".$veloce[0]->getTitolo()." - Giorni: <strong>".intval($veloce[1])."</strong>" ?>
   <?php echo "<br/>" ?>
 <?php endforeach ?> 
 <br /><br />
