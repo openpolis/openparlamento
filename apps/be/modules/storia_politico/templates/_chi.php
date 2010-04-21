79005fb6d04de880345914fa824c540e1c5d0e09
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-04-21 14:05:56 +0000

    aggiunto link a pagina politico in interfaccia ranking nuovo indice be
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@816 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/be/modules/storia_politico/templates/_chi.php
+++ apps/be/modules/storia_politico/templates/_chi.php
@@ -1,4 +1,8 @@
 <?php if ($opp_politician_history_cache->getChiTipo() == 'P'): ?>
   <?php $parlamentare = OppCaricaPeer::retrieveByPK($opp_politician_history_cache->getChiId()) ?>
-  <?php echo $parlamentare->getOppPolitico() ?> - <?php echo $parlamentare->getPoliticoId() ?> (<?php echo $parlamentare->getId() ?>)  
+  <?php echo $parlamentare->getOppPolitico() ?> - 
+  <?php echo link_to($parlamentare->getPoliticoId(), 
+                     'http://' . sfConfig::get('sf_site_url', 'op_openpolis') . "/parlamentare/" . $parlamentare->getPoliticoId(),
+                     true) ?>
+  (<?php echo $parlamentare->getId() ?>)  
 <?php endif ?>
