b3d555bfc3eefd07cec9951f4810bd9afbfb54c3
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-04-21 10:01:42 +0000

    i loop sui politici vanno a step di 50 per limitare l'uso della memoria
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@814 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/calcolo_nuovo_indice.sh
+++ batch/calcolo_nuovo_indice.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 # calcolo indice complessivo per i politici di camera e senato
-for i in 0 100 200 300 400 500 600; do ./symfony opp-calcola-indice --ramo=camera --offset=$i --limit=100; done;
-for i in 0 100 200 300 400; do ./symfony opp-calcola-indice --ramo=senato --offset=$i --limit=100; done;
+for (( i = 0 ; i < 630 ; i += 50 )) 
+do 
+  ./symfony opp-calcola-indice --ramo=camera --offset=$i --limit=50
+done
+
+for (( i = 0; i < 350 ; i += 30 ))
+do
+  ./symfony opp-calcola-indice --ramo=senato --offset=$i --limit=30
+done
+
