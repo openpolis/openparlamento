ac5e9958644dda4986669c1cbcbe98531f0fa5b1
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-06-17 12:33:02 +0000

    script per normalizzazione tag
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@943 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ batch/utility/listaTagConNumeroAttiEmendamentiAssociati.php
@@ -0,0 +1,41 @@
+<?php
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/../..'));
+define('SF_APP',         'fe');
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       true);
+ 
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+sfContext::getInstance();
+
+/**
+ * Lo script prepara la normaliazzione dei tag di openparlamento
+ *
+ */
+ 
+ // estraggo tutti i tag di opp con il numero di atti (no emendamenti) associati
+$tagToDdl=TaggingPeer::CountTagForAtti('OppAtto');
+$tagToEm=TaggingPeer::CountTagForAtti('OppEmedamento');
+
+$tagToAll=array();
+foreach ($tagToDdl as $key => $value) {
+  if (array_key_exists($key,$tagToEm)) {
+    $tagToAll[$key]=array($value[0],$value[1], $tagToEm[$key][1]);
+  }
+  else {
+    $tagToAll[$key]=array($value[0],$value[1], 0);
+  }
+}
+
+foreach ($tagToEm as $key => $value) {
+  if (!array_key_exists($key,$tagToAll)) {
+    $tagToAll[$key]=array($value[0],0,$value[1]);
+  }
+}
+
+foreach ($tagToAll as $key => $value) {
+  echo $key.",".$value[0].",".$value[1].",".$value[2]."\n";
+}
+ 
+
+
+?>
