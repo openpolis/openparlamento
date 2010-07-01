386ad4ce5fb6af33cb2825521c34325e67c936a1
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-01 10:26:48 +0000

    modificato stile liste in content dei post del blog
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@956 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- data/sql/20100511_add_id_to_cache_tables.sql
+++ data/sql/20100511_add_id_to_cache_tables.sql
@@ -1,3 +1,6 @@
-ALTER TABLE opp_politician_history_cache DROP PRIMARY KEY, ADD ID INTEGER NOT NULL AUTO_INCREMENT KEY FIRST;
-ALTER TABLE opp_act_history_cache DROP PRIMARY KEY, ADD ID INTEGER NOT NULL AUTO_INCREMENT KEY FIRST;
-ALTER TABLE opp_tag_history_cache DROP PRIMARY KEY, ADD ID INTEGER NOT NULL AUTO_INCREMENT KEY FIRST;
+ALTER TABLE opp_politician_history_cache DROP PRIMARY KEY;
+ALTER TABLE opp_act_history_cache DROP PRIMARY KEY; 
+ALTER TABLE opp_tag_history_cache DROP PRIMARY KEY; 
+ALTER TABLE opp_politician_history_cache ADD id INTEGER NOT NULL AUTO_INCREMENT KEY FIRST, ADD UNIQUE INDEX data_tipo_id_ramo (data, chi_tipo, chi_id, ramo);
+ALTER TABLE opp_act_history_cache ADD id INTEGER NOT NULL AUTO_INCREMENT KEY FIRST, ADD UNIQUE INDEX data_tipo_id (data, chi_tipo, chi_id);
+ALTER TABLE opp_tag_history_cache ADD id INTEGER NOT NULL AUTO_INCREMENT KEY FIRST, ADD UNIQUE INDEX data_tipo_id (data, chi_tipo, chi_id);
