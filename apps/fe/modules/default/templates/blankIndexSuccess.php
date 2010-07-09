80208e44847e5e8a7c4900d37d6979b2d5fd408d
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-09 10:07:09 +0000

    modificato messaggio per sciopero
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@989 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/default/templates/blankIndexSuccess.php
+++ apps/fe/modules/default/templates/blankIndexSuccess.php
@@ -1,1 +1,4 @@
-<div style="width: 95%; text-align: center; margin-top: 200px; margin-left: auto; margin-right: auto">Openpolis aderisce allo sciopero indetto dalla Federazione nazionale della stampa contro la legge sulle intercettazioni</div>
+<div style="width: 95%; text-align: center; margin-top: 100px; margin-left: auto; margin-right: auto; font-family:Arial, Helvetica, Sans-serif; font-size: 11px;">
+  <img src="/images/openpolis-post-it-small.jpg" /><br/>
+  <a href="/default/index">accedi al sito</a>
+</div>
