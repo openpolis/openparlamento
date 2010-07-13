2dd192dab7040003d209f3a8151ae8f2d64d269e
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-13 20:40:31 +0000

    modificato template _chi, per visualizzare info anche per chi_tipo=N
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1022 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/be/modules/storia_politico/templates/_chi.php
+++ apps/be/modules/storia_politico/templates/_chi.php
@@ -1,4 +1,5 @@
-<?php if ($opp_politician_history_cache->getChiTipo() == 'P'): ?>
+<?php if ($opp_politician_history_cache->getChiTipo() == 'P' || 
+          $opp_politician_history_cache->getChiTipo() == 'N'): ?>
   <?php $parlamentare = OppCaricaPeer::retrieveByPK($opp_politician_history_cache->getChiId()) ?>
   <a href="/xml/indici/politici/<?php echo $parlamentare->getId() ?>.xml" target="_blank">
     <?php echo $parlamentare->getOppPolitico() ?>
