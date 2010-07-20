645ff12a112bfae633104a5a70ae444f1a179704
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-20 14:17:46 +0000

    modificato script di update indici; gli indici sono cancellati prima di essere aggiornati; questo evita che vengano visualizzati dei risultati inesistenti nella nostra base dati
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1043 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/updateIndexes.sh
+++ batch/updateIndexes.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+
+php symfony solr-delete-propel-model fe OppPolitico
 php symfony solr-update-propel-model fe OppPolitico 1000
+
+php symfony solr-delete-propel-model fe OppVotazione
 php symfony solr-update-propel-model fe OppVotazione 200
+
+php symfony solr-delete-propel-model fe OppDocumento
 php symfony solr-update-propel-model fe OppDocumento 200
+
+php symfony solr-delete-propel-model fe OppAtto
 php symfony solr-update-propel-model fe OppAtto 500
+
+php symfony solr-delete-propel-model fe OppEmendamento
 php symfony solr-update-propel-model fe OppEmendamento 1000
+
+php symfony solr-delete-propel-model fe OppResoconto
 php symfony solr-update-propel-model fe OppResoconto 500
+
+php symfony solr-delete-propel-model fe Tag
 php symfony solr-update-propel-model fe Tag 1000
+
