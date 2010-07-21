a6228bded50929a8a4e2caf9ed2fe46062d9626c
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-21 09:59:26 +0000

    resuscitati i partial che contengono l'elenco dei senatori e dei deputati che si occupano di qualcosa
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1047 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ apps/fe/modules/argomento/templates/_deputatisioccupanodi.php
@@ -0,0 +1,30 @@
+<?php echo use_helper('Parlamentare') ?>
+
+<?php if (count($politici) > 0): ?>
+ <div class="evidence-box float-container">
+ 
+  <h5 class="subsection">deputati che si occupano di questo argomento</h5>
+   <div class="pad10">
+    
+  	<ul>
+  	  <?php foreach ($politici as $carica_id => $relevance): ?>
+  	    <li style="font-size:12px; padding:5px 0 0 0;">
+  	     <?php echo link_to_politicoNomeTipoFromCaricaId($carica_id, $relevance); ?>
+  	    </li>
+  	  <?php endforeach ?>
+          <?php if ($n_remaining_politici > 0): ?>
+          
+             <li style="font-size:12px; padding:5px 0 0 0;">
+            ... e altri <?php echo $n_remaining_politici ?> deputati
+            </li>
+          <?php endif ?>
+          <li>&nbsp;</li>
+       </ul>
+    
+  </div> 
+ </div>
+ 
+
+<?php endif ?>
+
+
