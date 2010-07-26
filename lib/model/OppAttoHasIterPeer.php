111a482681a2a79148e08b69d5cbff2338c4592f
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-07-26 11:44:22 +0000

    l'iter concluso (id=28) non è considerato tra quelli che assegnano punti, perché nei casi di interpellanze o interrogazioni concluse, ci sono altre fasi che indicano la conclusione dell'atto
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1058 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- lib/model/OppAttoHasIterPeer.php
+++ lib/model/OppAttoHasIterPeer.php
@@ -42,7 +42,7 @@ class OppAttoHasIterPeer extends BaseOppAttoHasIterPeer
   public function getItineraAttoDataRS($atto_id, $data)
   {
 		$con = Propel::getConnection(self::DATABASE_NAME);
-    $sql = sprintf("select ai.data, i.id as iter_id from opp_iter i, opp_atto_has_iter ai where ai.atto_id=%d and ai.data < '%s' and ai.iter_id=i.id and i.id in (4, 7, 58, 57, 54, 52, 36, 30, 22, 25, 20, 16, 11, 13, 28, 40, 41, 42, 45, 50, 56) group by iter_id;",
+    $sql = sprintf("select ai.data, i.id as iter_id from opp_iter i, opp_atto_has_iter ai where ai.atto_id=%d and ai.data < '%s' and ai.iter_id=i.id and i.id in (4, 7, 58, 57, 54, 52, 36, 30, 22, 25, 20, 16, 11, 13, 40, 41, 42, 45, 50, 56) group by iter_id;",
                    $atto_id, $data);
     $stm = $con->createStatement(); 
     return $stm->executeQuery($sql, ResultSet::FETCHMODE_ASSOC);
