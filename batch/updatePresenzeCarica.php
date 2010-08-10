257fb8abbb243135438a2de3aa2f78533f732d80
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-08-10 16:01:34 +0000

    test debug
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1101 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- batch/updatePresenzeCarica.php
+++ batch/updatePresenzeCarica.php
@@ -1,7 +1,7 @@
 <?php
 define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
 define('SF_APP',         'fe');
-define('SF_ENVIRONMENT', 'prod');
+define('SF_ENVIRONMENT', 'dev');
 define('SF_DEBUG',       true);
  
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
@@ -13,77 +13,84 @@ print("Fetching data... \n");
 
 // DEPUTATI E SENATORI
 $c = new Criteria();
-$crit0 = $c->getNewCriterion(OppCaricaPeer::LEGISLATURA, $leg);
-$crit1 = $c->getNewCriterion(OppCaricaPeer::TIPO_CARICA_ID, 1);
-$crit2 = $c->getNewCriterion(OppCaricaPeer::TIPO_CARICA_ID, 4);
+$crit0 = $c->getNewCriterion(OppCaricaPeer::TIPO_CARICA_ID, 1);
+$crit1 = $c->getNewCriterion(OppCaricaPeer::TIPO_CARICA_ID, 4);
+
+$crit0->addOr($crit1);
+$crit2 = $c->getNewCriterion(OppCaricaPeer::LEGISLATURA, $leg);
+
+$crit0->addAnd($crit2);
 $crit3 = $c->getNewCriterion(OppCaricaPeer::TIPO_CARICA_ID, 5);
+$crit4 = $c->getNewCriterion(OppCaricaPeer::DATA_FINE, NULL, Criteria::ISNULL);
 
-$crit1->addOr($crit2);
-$crit1->addOr($crit3);
+$crit3->addAnd($crit4);
 
-$crit0->addAnd($crit1);
+$crit0->addOr($crit3);
 
 $c->add($crit0);
+$c->add(OppCaricaPeer::ID,332779);
+
 $cariche = OppCaricaPeer::doSelect($c);
 
-foreach ($cariche as $carica) {
-	$pres=0;
-	$ass=0;
-	$miss=0;
 
+
+foreach ($cariche as $carica) 
+{
+	  
+  $c=new Criteria();
+  $c->add(OppCaricaHasGruppoPeer::CARICA_ID,$carica->getId());
+  $rs=OppCaricaHasGruppoPeer::doSelect($c);
+  foreach ($rs as $r)
+  {
+    $r->setPresenze(0);
+    $r->save();
+    
+
+  	$c = new Criteria();
+  	$c->addJoin(OppVotazioneHasCaricaPeer::VOTAZIONE_ID,OppVotazionePeer::ID);
+  	$c->addJoin(OppVotazionePeer::SEDUTA_ID,OppSedutaPeer::ID);
+  	if ($r->getDataFine()!==NULL)
+  	{
+  	  $crit0 = $c->getNewCriterion(OppSedutaPeer::DATA, $r->getDataInizio(), Criteria::GREATER_EQUAL);
+      $crit1 = $c->getNewCriterion(OppSedutaPeer::DATA, $r->getDataFine(), Criteria::LESS_THAN);
+      $crit0->addAnd($crit1);
+      $c->add($crit0);
+  	}
+  	else
+  	  	$c->add(OppSedutaPeer::DATA,$r->getDataInizio(),Criteria::GREATER_EQUAL);
+
+  	  
+  	$c->add(OppVotazioneHasCaricaPeer::CARICA_ID,$carica->getId());
+  	$c->add(OppVotazioneHasCaricaPeer::VOTO,array('Assente','In missione', 'Votazione annullata'), Criteria::NOT_IN);
+  	
+  	echo $pres1 = OppVotazioneHasCaricaPeer::doCount($c);
+  	echo "\n";
+    
+    $r->setPresenze($pres1);
+    $r->save();
+  }
+  
+  $c = new Criteria();
+	$c->add(OppVotazioneHasCaricaPeer::CARICA_ID,$carica->getId());
+	$c->add(OppVotazioneHasCaricaPeer::VOTO,array('Assente','In missione', 'Votazione annullata'), Criteria::NOT_IN);
+	$pres = OppVotazioneHasCaricaPeer::doCount($c);	
 	$c = new Criteria();
 	$c->add(OppVotazioneHasCaricaPeer::CARICA_ID,$carica->getId());
-	$voti = OppVotazioneHasCaricaPeer::doSelect($c);
-	foreach ($voti as $voto) {
-			if ($voto->getVoto()=='Assente') $ass=$ass+1;
-			else {
-				if ($voto->getVoto()=='In missione') $miss=$miss+1;
-				else {
-					if ($voto->getVoto()!='Votazione annullata')
-						$pres=$pres+1;
-				}		
-			}
-	} 
+	$c->add(OppVotazioneHasCaricaPeer::VOTO,'Assente');
+	$ass = OppVotazioneHasCaricaPeer::doCount($c);
+	$c = new Criteria();
+	$c->add(OppVotazioneHasCaricaPeer::CARICA_ID,$carica->getId());
+	$c->add(OppVotazioneHasCaricaPeer::VOTO,'In missione');
+	$miss = OppVotazioneHasCaricaPeer::doCount($c);
+	
 	$carica->setPresenze($pres);
 	$carica->setAssenze($ass);
 	$carica->setMissioni($miss);
-	$carica->save();
-					
+	$carica->save();	
+				
 	echo $carica->getOppPolitico()->getCognome()." pres. ".$pres." ass. ".$ass." miss. ".$miss."\n";
 }
 
-//SENATORI A VITA
-
-echo "== sen. a vita == \n";
-$c = new Criteria();
-$c->add(OppCaricaPeer::DATA_FINE, NULL);
-$c->add(OppCaricaPeer::TIPO_CARICA_ID, 5);
-
-$cariche = OppCaricaPeer::doSelect($c);
-foreach ($cariche as $carica) {
-	$pres=0;
-	$ass=0;
-	$miss=0;
-
-	$c = new Criteria();
-	$c->add(OppVotazioneHasCaricaPeer::CARICA_ID,$carica->getId());
-	$voti = OppVotazioneHasCaricaPeer::doSelect($c);
-	foreach ($voti as $voto) {
-		if ($voto->getOppVotazione()->getOppSeduta()->getLegislatura()==$leg) {
-			if ($voto->getVoto()=='Assente') $ass=$ass+1;
-			else {
-				if ($voto->getVoto()=='In missione') $miss=$miss+1;
-				else $pres=$pres+1;
-			}
-		}	
-	} 
-	$carica->setPresenze($pres);
-	$carica->setAssenze($ass);
-	$carica->setMissioni($miss);
-	$carica->save();
-					
-	echo $carica->getOppPolitico()->getCognome()." pres. ".$pres." ass. ".$ass." miss. ".$miss."\n";
-}
 
 mail("e.dicesare@depp.it", "OK - Update Presenze", "aggiornamento a buon fine", "From: BatchOpp");
 
