96af929796fa4972fc0d663924309773b3f896ba
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-08-31 16:21:24 +0000

    ctrl newg con tag assaciati inesistenti
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1124 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ batch/utility/controlla_news_con_tag_che_non_esistono.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+controlla le news che fanno riferimento a tag che non esistono e le cancella
+*/
+ 
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/../..'));
+define('SF_APP',         'fe');
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       false);
+ 
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+sfContext::getInstance();
+
+
+$c= new Criteria();
+$c->add(NewsPeer::RELATED_MONITORABLE_MODEL,'Tag');
+$news=TagPeer::doSelect($c);
+foreach ($news as $n)
+{
+  $tag=TagPeer::retrieveByPk($n->getRelatedMonitorableId());
+  if (!$tag)
+    echo "tag id=".$n->getRelatedMonitorableId()." non esiste\n";
+}
+
+?>
