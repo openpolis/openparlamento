98f3e17674fa3bb96601c9678059eff3fc9bc92f
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-09-29 13:05:32 +0000

    test gruppi parlamentari
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1182 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/parlamentare/templates/_gruppiParlamentari.php
+++ apps/fe/modules/parlamentare/templates/_gruppiParlamentari.php
@@ -1,14 +1,11 @@
 <table class="disegni-decreti column-table lazyload">
   <thead>
     <tr>
-      <th scope="col">parlamentare:</th>
-      <th scope="col">indice di attivit&agrave;:</th> 	
-      <th scope="col">voti ribelli:</th>			
-      <th scope="col" class="evident">presenze:</th>			
-      <th scope="col" class="evident">assenze:</th>
-      <th scope="col" class="evident">missioni:</th>
-      <th scope="col">circoscrizione:</th>
-      <th scope="col">utenti che lo seguono:</th>
+      <th scope="col">Gruppo:</th>
+      <th scope="col">Attuali:</th> 	
+      <th scope="col">Entranti:</th>
+      <th scope="col">Uscenti:</th>
+      <th scope="col">Saldo:</th>
     </tr>
   </thead>
 
@@ -18,25 +15,59 @@ foreach ($gruppo_now as $k => $g)
 {
   if ($g!==0)
   {
-    echo OppGruppoPeer::retrieveByPk($k)->getNome()." Ora: ".$g." entranti: ".$gruppo_in[$k]." uscenti: ".$gruppo_out[$k]." saldo:".($gruppo_in[$k]-$gruppo_out[$k]);
-    echo "<br/>";
+    echo "<tr>";
+    echo "<td>".OppGruppoPeer::retrieveByPk($k)->getNome()."</td>";
+    echo "<td>".$g."</td>";
+    echo "<td>".$gruppo_in[$k]."</td>";
+    echo "<td>".$gruppo_out[$k]."</td>";
+    echo "<td>".($gruppo_in[$k]-$gruppo_out[$k])."</td>";
+    echo "</tr>";
   }
   
 }
+?>
+ </tbody>
+</table> 
 
+<table border=1>
+  <tbody>
+  <tr>
+  <td></td>  
+<?php
 echo "<br/><br/><br/>";
 
 foreach($array_diff as $k => $diff)
 {
-  echo OppGruppoPeer::retrieveByPk($k)->getNome();
-   echo "<br/>";
+  $pos[]=$k;
+  echo "<td>".OppGruppoPeer::retrieveByPk($k)->getAcronimo()."</td>";
+}
+echo "</tr>";  
+
+foreach($array_diff as $k => $diff)
+{
+  echo "<tr><td>".OppGruppoPeer::retrieveByPk($k)->getAcronimo()."</td>";
+  echo "=====".$k."===============\n";
+  var_dump($diff);
+  echo "<br/>";
+  for ($x=0;$x<count($array_diff);$x++)
+  {
+
+    if (array_key_exists($pos[$x],$diff))
+      echo "<td>".$diff[$pos[$x]]."</td>";
+    else
+      echo "<td>0</td>";
+   
+  }
+  echo "</tr>";
+  /*
   foreach ($diff as $k1 => $d)
   {
     echo OppGruppoPeer::retrieveByPk($k1)->getNome()."*".$d;
      echo "<br/>";
   }
-  echo "<br/><br/><br/>";
+  */
 }
 
-
 ?>
+</tbody>
+</table>
