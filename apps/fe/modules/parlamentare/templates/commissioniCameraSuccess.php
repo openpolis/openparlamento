7edf77da8aac0d69b586632bddaa8c1601bab27a
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-10-15 17:58:12 +0000

    ref pagina commisisoni
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1236 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/parlamentare/templates/commissioniCameraSuccess.php
+++ apps/fe/modules/parlamentare/templates/commissioniCameraSuccess.php
@@ -1,6 +1,5 @@
 <div id="content" class="tabbed float-container">
   <div id="main">
-
   <?php foreach ($comms as $comm) : ?>
     <?php echo include_component('parlamentare','commissioniPermanenti',array('sede_id' => $comm->getId(),'leg' => 16)) ?>
   <?php endforeach; ?>  
