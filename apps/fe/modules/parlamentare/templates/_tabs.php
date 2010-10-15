ef8d8de2c28f05ca5c5d791c7994c4ca8393c12c
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-10-15 18:25:35 +0000

    corretto routing per pagine commissioni camera e gruppi
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1238 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/parlamentare/templates/_tabs.php
+++ apps/fe/modules/parlamentare/templates/_tabs.php
@@ -3,7 +3,7 @@
     <h2><?php echo link_to('Deputati', '@parlamentari?ramo=camera') ?></h2>   
   </li>
   <li class="<?php echo($ramo==1 && $gruppi ? 'current' : '' ) ?>">
-    <h6><?php echo link_to('Gruppi Camera', '/camera/GruppiCamera') ?></h6>   
+    <h6><?php echo link_to('Gruppi Camera', '/gruppi_camera') ?></h6>   
   </li>
   <li class="<?php echo($ramo==1 && $compare ? 'current' : '' ) ?>">
     <h6><?php echo link_to('Confronta deputati', '/parlamentare/comparaDeputati?id1=4573&id2=161&ramo=1') ?></h6>   
@@ -12,7 +12,7 @@
     <h2><?php echo link_to('Senatori', '@parlamentari?ramo=senato') ?></h2>   
   </li>
   <li class="<?php echo($ramo==2 && $gruppi ? 'current' : '' ) ?>">
-    <h6><?php echo link_to('Gruppi Senato', '/senato/GruppiSenato') ?></h6>   
+    <h6><?php echo link_to('Gruppi Senato', '/gruppi_senato') ?></h6>   
   </li>
 <li class="<?php echo($ramo==2 && $compare ? 'current' : '' ) ?>">
     <h6><?php echo link_to('Confronta senatori', '/parlamentare/comparaDeputati?id1=303095&id2=332961&ramo=2') ?></h6>   
