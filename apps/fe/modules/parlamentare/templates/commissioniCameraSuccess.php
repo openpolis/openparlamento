7f65cca487517ad60aacb45127fbd3491240b17d
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-10-16 01:54:02 +0000

    add lista membri e ddl in pagina commissioni
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1246 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/parlamentare/templates/commissioniCameraSuccess.php
+++ apps/fe/modules/parlamentare/templates/commissioniCameraSuccess.php
@@ -2,8 +2,6 @@
   <div id="main">
   <?php foreach ($comms as $comm) : ?>
     <?php echo include_component('parlamentare','commissioniPermanenti',array('sede_id' => $comm->getId(),'leg' => 16)) ?>
-    
-    
   <?php endforeach; ?>  
   </div>
 </div>              
