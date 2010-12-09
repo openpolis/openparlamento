99659a67d878d297957da28b82be697760046e55
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2010-12-09 17:02:52 +0000

    implementato l'indicator per le azioni ajax; implementata la security: solo gli utenti amministratori o adhoc visualizzano linterfaccia per il voto
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1418 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/parlamentare/templates/_interventiList.php
+++ apps/fe/modules/parlamentare/templates/_interventiList.php
@@ -8,8 +8,12 @@
       <th scope="col">data:</th>
       <th scope="col">sede:</th>
       <th scope="col">atto:</th>  
+      <th scope="col">link:</th>  
+      
+      <?php if ($sf_user->isAuthenticated() && $sf_user->hasCredential('amministratore')): ?>
       <th scope="col">parere utenti:</th>  
       <th scope="col">il tuo parere:</th>
+      <?php endif ?>
     </tr>
   </thead>
 
@@ -35,12 +39,16 @@
          </p>
           <p>
             <?php echo link_to(Text::denominazioneAtto($atto, 'list'), 'atto/index?id='.$atto->getId()) ?>
-            <?php echo link_to(image_tag('extlink.gif',
-                                         array('title' => 'Vai all\'intervento sul sito '.($intervento->getOppSede()->getRamo() == 'C'?'della Camera':'del Senato'))) ,
-                              (preg_match("#^http:#",$intervento->getUrl()) ? $intervento->getUrl() : sfConfig::get('app_url_sito_camera', 'http://nuovo.camera.it/').$intervento->getUrl()),
-                              array('class' => 'external-url-container') ) ?>
           </p>
         </th>
+        <td>          
+          <?php echo link_to(image_tag('extlink.gif',
+                                       array('title' => 'Vai all\'intervento sul sito '.($intervento->getOppSede()->getRamo() == 'C'?'della Camera':'del Senato'))) ,
+                            (preg_match("#^http:#",$intervento->getUrl()) ? $intervento->getUrl() : sfConfig::get('app_url_sito_camera', 'http://nuovo.camera.it/').$intervento->getUrl()),
+                            array('class' => 'external-url-container') ) ?>
+        </td>
+        
+        <?php if ($sf_user->isAuthenticated() && ($sf_user->hasCredential('amministratore') || $sf_user->hasCredential('adhoc'))): ?>
         <td>
           <div class="user-stats-column">
             <?php include_component('deppVoting', 'votingDetailsSmall', array('object' => $intervento)) ?>
@@ -51,16 +59,18 @@
             <?php include_component('deppVoting', 'votingBlockSmall', array('object' => $intervento)) ?>            
           </div>
         </td>
+        <?php endif ?>
       </tr>
     <?php endforeach; ?>
+    
     <tr>
-      <td align="center" colspan='5'>
+      <td align="center" colspan='6'>
         <?php echo pager_navigation($pager, '@parlamentare_interventi?id='.$parlamentare_id) ?>
       </td>
     </tr>
 
     <tr>
-      <td align="center" colspan='5'>
+      <td align="center" colspan='6'>
         <?php echo format_number_choice('[0] nessun risultato|[1] 1 risultato|(1,+Inf] %1% risultati', array('%1%' => $pager->getNbResults()), $pager->getNbResults()) ?>
       </td>
     </tr>
@@ -74,128 +84,152 @@
 </div>
 <div id="backgroundPopup"></div>
 
-<!-- slider jQuery per gli atti e le notizie relative -->
-<script type="text/javascript">
-//<![CDATA[
+<?php if ($sf_user->isAuthenticated() && ($sf_user->hasCredential('amministratore') || $sf_user->hasCredential('adhoc'))): ?>
+  <!-- slider jQuery per gli atti e le notizie relative -->
+  <script type="text/javascript">
+  //<![CDATA[
 
-jQuery.noConflict();
-(function($) {
-  $().ready(function(){
+  jQuery.noConflict();
+  (function($) {
+    $().ready(function(){
 
-    var popupStatus = 0;
-    
-    //loading popup with jQuery magic!
-    function loadPopup(page_url){
-      //loads popup only if it is disabled
-      if(popupStatus==0){
+      var popupStatus = 0;
+
+      //loading popup with jQuery magic!
+      function loadPopup(page_url){
+        //loads popup only if it is disabled
+        if(popupStatus==0){
+          $("#backgroundPopup").css({
+            "opacity": "0.7"
+          });
+          $("#backgroundPopup").fadeIn("slow");
+          $("#popupContent").fadeIn("slow");
+          $("#contentArea").attr("src", page_url);
+          popupStatus = 1;
+        }
+      }
+
+      //disabling popup with jQuery magic!
+      function disablePopup(){
+        //disables popup only if it is enabled
+        if(popupStatus==1){
+          $("#backgroundPopup").fadeOut("slow");
+          $("#popupContent").fadeOut("slow");
+          popupStatus = 0;
+        }
+      }    
+
+      //centering popup
+      function centerPopup(){
+        //request data for centering
+        var windowWidth = document.documentElement.clientWidth;
+        var windowHeight = document.documentElement.clientHeight;
+        var popupHeight = $("#popupContent").height();
+        var popupWidth = $("#popupContent").width();
+        //centering
+        $("#popupContent").css({
+          "position": "fixed",
+          "top": windowHeight/2-popupHeight/2,
+          "left": windowWidth/2-popupWidth/2
+        });
+        //only need force for IE6
+
         $("#backgroundPopup").css({
-          "opacity": "0.7"
+          "height": windowHeight
         });
-        $("#backgroundPopup").fadeIn("slow");
-        $("#popupContent").fadeIn("slow");
-        $("#contentArea").attr("src", page_url);
-        popupStatus = 1;
+
       }
-    }
 
-    //disabling popup with jQuery magic!
-    function disablePopup(){
-      //disables popup only if it is enabled
-      if(popupStatus==1){
-        $("#backgroundPopup").fadeOut("slow");
-        $("#popupContent").fadeOut("slow");
-        popupStatus = 0;
-      }
-    }    
-
-    //centering popup
-    function centerPopup(){
-      //request data for centering
-      var windowWidth = document.documentElement.clientWidth;
-      var windowHeight = document.documentElement.clientHeight;
-      var popupHeight = $("#popupContent").height();
-      var popupWidth = $("#popupContent").width();
-      //centering
-      $("#popupContent").css({
-        "position": "fixed",
-        "top": windowHeight/2-popupHeight/2,
-        "left": windowWidth/2-popupWidth/2
-      });
-      //only need force for IE6
-
-      $("#backgroundPopup").css({
-        "height": windowHeight
+      $('a.external-url-container').click(function(){
+    		var page_url = $(this).attr('href');
+    	  centerPopup();
+    		loadPopup(page_url);
+    		return false;
       });
 
-    }
-
-    $('a.external-url-container').click(function(){
-  		var page_url = $(this).attr('href');
-  	  centerPopup();
-  		loadPopup(page_url);
-  		return false;
-    });
-
-    //Click out event!  
-    $("#backgroundPopup").click(function(){  
-      disablePopup();  
-    });  
-    
-    $("#popupContentClose").click(function(){
-      disablePopup();
-      return false;
-    });
-    
-    $(".clickable").click(function(){
-      if($(this).css('background-color') != 'rgb(255, 238, 238)')
-      {
-        $(this).css({
-          "background-color": "#ffeeee"
-        });        
-      } else {
-        if($(this).attr('class') == 'clickable even')
-          $(this).css('background-color', '#ffffff');
-        else
-          $(this).css('background-color', '#fafafa');
-      }
-    });
-
-    //Press Escape event!  
-    $(document).keypress(function(e){  
-      if(e.keyCode==27 && popupStatus==1){  
+      //Click out event!  
+      $("#backgroundPopup").click(function(){  
         disablePopup();  
-      }
-    });
+      });  
 
-    
-    // behavior per gestire richieste ajax al click su link
-    function bind_voting_behavior(scope){
-      $('a', scope).click(function(e){
-        e.preventDefault();
-
-        var table_row = $(this).parents('tr');
-        var user_stats_col = table_row.find('.user-stats-column');
-        var user_vote_col = table_row.find('.user-vote-column');
-        var token = user_stats_col.find('div').attr('id');
-
-        // send request
-        $.get($(this).attr('href'), function(data) {
-          user_vote_col.html(data);
-          bind_voting_behavior(user_vote_col);
-          user_stats_col.load("/deppVoting/votingDetails?type=small&token="+token);
-        });
-        
+      $("#popupContentClose").click(function(){
+        disablePopup();
+        return false;
       });
-    };
-    
-    // associazine del behavior ai link della colonna dei voti
-    bind_voting_behavior($('.user-vote-column'));
-      
 
-    
+      $(".clickable").click(function(){
+        if($(this).css('background-color') != 'rgb(255, 238, 238)')
+        {
+          $(this).css({
+            "background-color": "#ffeeee"
+          });        
+        } else {
+          if($(this).attr('class') == 'clickable even')
+            $(this).css('background-color', '#ffffff');
+          else
+            $(this).css('background-color', '#fafafa');
+        }
+      });
 
-  })
-})(jQuery);
+      //Press Escape event!  
+      $(document).keypress(function(e){  
+        if(e.keyCode==27 && popupStatus==1){  
+          disablePopup();  
+        }
+      });
 
-//]]>
-</script>
+
+      // behavior per gestire richieste ajax al click su link
+      function bind_voting_behavior(scope){
+        $('a', scope).click(function(e){
+          e.preventDefault();
+
+          var table_row = $(this).parents('tr');
+          var user_stats_col = table_row.find('.user-stats-column');
+          var user_vote_col = table_row.find('.user-vote-column');
+          var token = user_stats_col.find('div').attr('id');
+
+          // send request
+          $.get($(this).attr('href'), function(data) {
+            user_vote_col.html(data);
+            bind_voting_behavior(user_vote_col);
+            user_stats_col.load("/deppVoting/votingDetails?type=small&token="+token);
+          });
+
+        });
+      };
+
+      // associazine del behavior ai link della colonna dei voti
+      bind_voting_behavior($('.user-vote-column'));
+
+      // Setup the ajax indicator
+      $('body').append('<div id="ajaxBusy"><p><img src="/images/loading.gif"></p></div>');
+
+      $('#ajaxBusy').css({
+        display:"none",
+        margin:"0px",
+        paddingLeft:"0px",
+        paddingRight:"0px",
+        paddingTop:"0px",
+        paddingBottom:"0px",
+        position:"fixed",
+        left:"50%",
+        top:"50%",
+         width:"auto"
+      });
+
+      // Ajax activity indicator bound to ajax start/stop document events
+      $(document).ajaxStart(function(){ 
+        $('#ajaxBusy').show(); 
+      }).ajaxStop(function(){ 
+        $('#ajaxBusy').hide();
+      });
+
+
+
+    })
+  })(jQuery);
+
+  //]]>
+  </script>  
+<?php endif ?>
