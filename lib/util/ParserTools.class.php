5e2ba05341c23198cd2d80a47d9ed43c534e74d0
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2011-10-28 09:57:52 +0000

    aad class
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1702 f338ed83-fc16-40d3-870d-fec3a6a98b5b

---
+++ lib/util/ParserTools.class.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Some utility static methods for parsing
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class ParserTools
+{
+  /**
+   * transform an array of any objects into an array of Primary Keys
+   *
+   * @return void
+   * @author Guglielmo Celata
+   **/
+  public static function load_remote_html_page_into_dom($uri, $n_tentativi = 10, $timeout = 5)
+  {
+    include_once (SF_ROOT_DIR."/batch/parser/simple_html_dom.php");
+
+    $ctx = stream_context_create(array( 
+        'http' => array( 
+            'timeout' => $timeout
+            ) 
+        ) 
+    ); 
+    
+    for ($i = 1; $i <= $n_tentativi; $i++)
+    {
+  	  $html = file_get_html($uri, 0, $ctx);
+  	  if ($html != '')
+  	  {
+  	    break;
+  	  }
+    }
+    
+      return $html;
+    
+  } 
+}
