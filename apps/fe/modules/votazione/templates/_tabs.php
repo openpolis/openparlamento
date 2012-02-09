45cb04bd0eae238950531d222839be8392c5631a
ettore <ettore@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2012-02-09 10:56:20 +0000

    commentati tab per votazioni sotto e salva
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1753 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/votazione/templates/_tabs.php
+++ apps/fe/modules/votazione/templates/_tabs.php
@@ -3,13 +3,14 @@
   <li class="<?php echo($current == 'voti_chiave' ? 'current' : '' ) ?>">
     <h2><?php echo link_to('Voti chiave', '@votichiave') ?></h2>   
   </li>
- 
-  <li class="<?php echo($current == 'maggioranza_sotto' ? 'current' : '' ) ?>">
-    <h2><?php echo link_to('Maggioranza battuta', '@maggioranzaSotto') ?></h2>   
+ <!--
+  <li class="<?php //echo($current == 'maggioranza_sotto' ? 'current' : '' ) ?>">
+    <h2><?php //echo link_to('Maggioranza battuta', '@maggioranzaSotto') ?></h2>   
   </li>
   <li class="<?php echo($current == 'maggioranza_salva' ? 'current' : '' ) ?>">
-    <h2><?php echo link_to('Maggioranza salvata', '@maggioranzaSalva') ?></h2>   
+    <h2><?php //echo link_to('Maggioranza salvata', '@maggioranzaSalva') ?></h2>   
   </li>
+  -->
   <li class="<?php echo($current == 'voti_tutti' ? 'current' : '' ) ?>">
     <h2><?php echo link_to('Tutte le votazioni', '@votazioni') ?></h2>   
   </li>
