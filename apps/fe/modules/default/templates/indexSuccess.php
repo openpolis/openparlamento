0323e767d60e576d28cd8e8281430adfbf9dd857
guglielmo <guglielmo@f338ed83-fc16-40d3-870d-fec3a6a98b5b>
2013-02-04 16:02:38 +0000

    add banner cm2013
    git-svn-id: http://svn.openpolis.it/repos/openparlamento/trunk@1825 f338ed83-fc16-40d3-870d-fec3a6a98b5b

--- apps/fe/modules/default/templates/indexSuccess.php
+++ apps/fe/modules/default/templates/indexSuccess.php
@@ -16,7 +16,7 @@
 	<div class="sevencol">
            <!-- Banner Camere Aperte 2013 -->
    <!--       <div style="margin-bottom:20px;">
-    <?php //echo link_to(image_tag('ca2013_banner.png', array('alt' => 'Camere Aperte 2013')), 'http://feeds.feedburner.com/openpolis?format=xml') ?>
+    <?php //echo link_to(image_tag('ca2013_banner.png', array('alt' => 'Camere Aperte 2013')), 'https://s3-eu-west-1.amazonaws.com/camereaperte2013.openpolis.it/dossier_camereaperte2013.pdf') ?>
 </div> -->        
 
 	    <!-- in evidenza dal blog -->
