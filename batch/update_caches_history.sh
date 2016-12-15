7cd30d60f2e34fdb83ad9b89cabedc7f9d82edc7
Guglielmo <guglielmo.celata@gmail.com>
2016-12-15 13:44:53 +0100

    script to update historic caches creted

---
+++ batch/update_caches_history.sh
@@ -0,0 +1,43 @@
+# script valido per la 17. legislatura
+
+# primo anno
+year=2013
+for m in {4..12}
+   do
+     data=$(date -d "$year-01-01 +$m month -1 day" "+%Y-%m-%d")
+     echo $data
+     ./symfony opp-build-cache-politici --ramo=camera --data=$data
+     ./symfony opp-build-cache-politici --ramo=senato --data=$data
+     ./symfony opp-build-pos-cache-politici --ramo=camera --data=$data;
+     ./symfony opp-build-pos-cache-politici --ramo=senato --data=$data;
+     ./symfony opp-build-cache-gruppi --data=$data
+     ./symfony opp-build-cache-rami --data=$data
+   done
+
+for year in 2014 2015
+   do
+for m in {1..12}
+   do
+     data=$(date -d "$year-01-01 +$m month -1 day" "+%Y-%m-%d")
+     echo $data
+     ./symfony opp-build-cache-politici --ramo=camera --data=$data
+     ./symfony opp-build-cache-politici --ramo=senato --data=$data
+     ./symfony opp-build-pos-cache-politici --ramo=camera --data=$data;
+     ./symfony opp-build-pos-cache-politici --ramo=senato --data=$data;
+     ./symfony opp-build-cache-gruppi --data=$data
+     ./symfony opp-build-cache-rami --data=$data
+   done
+done
+
+year=2016
+for m in {1..11}
+   do
+     data=$(date -d "$year-01-01 +$m month -1 day" "+%Y-%m-%d")
+     echo $data
+     ./symfony opp-build-cache-politici --ramo=camera --data=$data
+     ./symfony opp-build-cache-politici --ramo=senato --data=$data
+     ./symfony opp-build-pos-cache-politici --ramo=camera --data=$data;
+     ./symfony opp-build-pos-cache-politici --ramo=senato --data=$data;
+     ./symfony opp-build-cache-gruppi --data=$data
+     ./symfony opp-build-cache-rami --data=$data
+   done
