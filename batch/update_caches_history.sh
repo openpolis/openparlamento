7d56584fd12a98257c46a522d8917631e88481c0
Guglielmo <guglielmo.celata@gmail.com>
2017-05-12 10:26:01 +0200

    history cache building script corrected

--- batch/update_caches_history.sh
+++ batch/update_caches_history.sh
@@ -30,7 +30,7 @@ for m in {1..12}
 done
 
 year=2016
-for m in {1..11}
+for m in {1..12}
    do
      data=$(date -d "$year-01-01 +$m month -1 day" "+%Y-%m-%d")
      echo $data
@@ -41,3 +41,18 @@ for m in {1..11}
      ./symfony opp-build-cache-gruppi --data=$data
      ./symfony opp-build-cache-rami --data=$data
    done
+
+year=2017
+for m in {1..3}
+   do
+     data=$(date -d "$year-01-01 +$m month -1 day" "+%Y-%m-%d")
+     echo $data
+     ./symfony opp-build-cache-politici --ramo=camera --data=$data
+     ./symfony opp-build-cache-politici --ramo=senato --data=$data
+     ./symfony opp-build-pos-cache-politici --ramo=camera --data=$data;
+     ./symfony opp-build-pos-cache-politici --ramo=senato --data=$data;
+     ./symfony opp-build-cache-gruppi --data=$data
+     ./symfony opp-build-cache-rami --data=$data
+   done
+
+
